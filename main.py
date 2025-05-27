13528a99c4916370eb88137d66696f6758ac86cd
Daniel Erenrich <daniel@erenrich.net>
2025-05-27 17:13:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,22 @@ def write_registers(i2c, regs, num=None):
     return i2c.writeto(I2C_ADDRESS, wr_regs)
     
 
+def read_rds(i2c, tries=300):
+    try_num = 0
+    while True:
+        regs = list(read_registers(i2c))
+        rds_ready = regs[0xA] & (1 << 15)
+        if rds_ready:
+            dat = (regs[0xC], regs[0xD], regs[0xE], regs[0xF], try_num)
+            return dat
+        # manual advises polling every ~40ms
+        time.sleep(0.040)
+        try_num += 1
+        if try_num > tries:
+            break
+    return None
+
+
 # startup sequence
 sdio_pin = machine.Pin(10, Pin.OUT)
 sdio_pin.value(0)
@@ -63,22 +79,86 @@ regs[0x2] = 1
 write_registers(i2c, shorts_to_bytearray(regs))
 time.sleep(1)
 
-device_register = read_registers(i2c)
-
-for i, b in enumerate(device_register):
-    print(i, hex(b))
-
-
-# seek
+# enable RDS
 regs = list(read_registers(i2c))
-regs[0x2] |= (1 << 8)
+regs[0x4] |= (1 << 12)
+write_registers(i2c, shorts_to_bytearray(regs))
+    
+    
+# configure seek params
+regs = list(read_registers(i2c))
+regs[0x5] = (regs[0x5] & 0xff) | (0x5F << 8)
 write_registers(i2c, shorts_to_bytearray(regs))
 
-time.sleep(10)
+# seek to a real chan
+while True:
+    #print("seeking")
+    regs = list(read_registers(i2c))
+    regs[0x2] |= (1 << 8)
+    write_registers(i2c, shorts_to_bytearray(regs))
+    time.sleep(0.10)
+    timeout = 0
+    lock = False
+    while True:
+        regs = list(read_registers(i2c))
+        seek_done = regs[0xA] & (1 << 14)
+        if seek_done:
+            print("seek done")
+            lock = True
+            break
+        
+        seek_fail = regs[0xA] & (1 << 13)
+        if seek_fail:
+            print("seek fail")
+            break
+        
+        afc_rail = regs[0xA] & (1 << 12)
+        #if afc_rail:
+        #    print("afc rail")
+        #    break
+        
+        chan = (regs[0xB] & 0b111111111)
+        freq_khz = chan * 200 + 87500
+        
+        rssi = regs[0xA] & 0xff
+        
+        time.sleep(1)
+        timeout += 1
+        #print(freq_khz, 'khz', rssi, 'db', hex(regs[0xA]))
+        if rssi > 28:
+            lock = True
+            #print(freq_khz, 'khz', rssi, 'db', hex(regs[0xA]))
+            break        
+        if timeout > 4:
+            break
+    if lock:
+        #print("seek done")
+        rds_dat = read_rds(i2c, 200)
+        
+        regs = list(read_registers(i2c))
+        chan = (regs[0xB] & 0b111111111)
+        freq_khz = chan * 200 + 87500        
+        rssi = regs[0xA] & 0xff
+        
+        if rds_dat:
+            PIC = rds_dat[0]
+            print(freq_khz, 'khz', rssi, 'db', hex(PIC), rds_dat)
+        else:
+            print(freq_khz, 'khz', rssi, 'db', "no rds")
 
-device_register = read_registers(i2c)
-for i, b in enumerate(device_register):
-    print(i, hex(b))
+    # clear seek bit
+    regs = list(read_registers(i2c))
+    regs[0x2] &= ~(1 << 8)
+    write_registers(i2c, shorts_to_bytearray(regs))
+    time.sleep(0.30)
+
+
+
+
+
+    
+
+
 
     
 
